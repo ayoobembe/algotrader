3fa3f7b25e9fbd5bcea4728f801eedfd7f2dc955
Ayo <ayo.obembe@gmail.com>
2015-11-19 14:52:58 +0000

    calculating indicator values and created array of indicators, vertically stacked

--- compute_indicators.py
+++ compute_indicators.py
@@ -14,14 +14,23 @@ inputs = {
     'volume': gbpusd[:,4]
 }
 
-moving_average_50  = EMA(inputs, timeperiod=50, price='close')
-moving_average_200 = EMA(inputs, timeperiod=200,price='close')
-stoch_oscillator = 
+moving_average_50  = np.array(EMA(inputs, timeperiod=50, price='close'))
+moving_average_200 = np.array(EMA(inputs, timeperiod=200,price='close'))
+slowk, slowd = np.array(STOCH(inputs, 5,3,0,3,0)) 
+bal_of_power = np.array(BOP(inputs))
+accum_dist = np.array(AD(inputs))
+
+indicatorArray = np.vstack((moving_average_50, moving_average_200, slowk, slowd, bal_of_power, accum_dist))
 
 
-print moving_average_50[48:54]
-print moving_average_200[198:204]
 
+print moving_average_50[60]
+print moving_average_200[60]
+print slowk[60]
+print slowd[60]
+print bal_of_power[60]
+print accum_dist[60]
+print indicatorArray[:,60]
 
 
 
